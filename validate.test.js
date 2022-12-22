a33261c0a1578fd82380197337ce8e4ccdfd05de
Ricardo Gonzales <ricardo.gonzales4@unmsm.edu.pe>
2022-12-22 15:44:56 -0500

    dockerfile

--- validate.test.js
+++
@@ -1,5 +0,0 @@
-const ValidacionToken = require('./Middlewares/authenticate')
-
-test('req authorization -> true, res 403 -> true, next a -> true', () =>{
-    expect(ValidacionToken.auth('authorization',403,'a')).toBe(true)
-});
